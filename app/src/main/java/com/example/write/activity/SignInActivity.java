a822dffb529b34fd1c264922804900cfebf7a375
wsunior <ksenya.myasoedova@gmail.com>
2021-01-27 17:25:20 +0300

    ver 2.0

--- app/src/main/java/com/example/write/activity/SignInActivity.java
+++ app/src/main/java/com/example/write/activity/SignInActivity.java
@@ -13,13 +13,7 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
-    }
-    public void SignUpActivity (android.view.View v) {
-        startActivity(
-                new Intent(
-                        SignInActivity.this,
-                SignUpActivity.class
-                )
-        );
+
+        findViewById(R.id.signInSwipe).setOnClickListener(v -> startActivity(new Intent(SignInActivity.this, SignUpActivity.class)));
     }
 }
